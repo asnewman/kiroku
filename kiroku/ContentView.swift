f7a44945836a443aa1598571304fedc71ea6564f
asnewman <ashznewman@gmail.com>
2025-07-13 09:27:48 -0700

    Initial Commit

---
+++ kiroku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kiroku
+//
+//  Created by Ashley Newman on 7/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kiroku/kirokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  kirokuApp.swift
+//  kiroku
+//
+//  Created by Ashley Newman on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct kirokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
